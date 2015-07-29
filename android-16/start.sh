637ea76050ddf804ca10f2959171c6897745c790
Bastien DAVID <bastien.david@orange.com>
2015-07-29 16:36:46 +0200

    Running arm64 emulator + VNC
    Emulators are now running in arm64, and expose a VNC port.

--- android-16/start.sh
+++ android-16/start.sh
@@ -2,8 +2,7 @@
 
 # Detect ip and forward ADB ports outside to outside interface
 ip=$(ip addr list eth0|grep "inet "|cut -d' ' -f6|cut -d/ -f1)
-#socat tcp-listen:5555,bind=$ip,fork tcp:127.0.0.1:5555 &
 redir --laddr=$ip --lport=5555 --caddr=127.0.0.1 --cport=5555 &
 
 # Set up and run emulator
-emulator -avd ANDROID -noaudio -no-window -gpu off -verbose -force-32bit
+emulator64-arm -avd ANDROID -noaudio -no-window -gpu off -verbose -qemu -vnc :0
